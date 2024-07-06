8e6cd1753905be39a35a11da3e88abd615e0ba43
BaduocUc <ba.aguirrem@duocuc.cl>
2024-07-06 03:54:52 -0400

    prueba

--- appShop/admin.py
+++ appShop/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import Category, Product, Foundation
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Foundation)
+
+
--- appShop/models.py
+++ appShop/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+
+class Category(models.Model):
+    id_category  = models.AutoField(db_column='idCategory', primary_key=True) 
+    category     = models.CharField(max_length=20, blank=False, null=False)
+    
+    def __str__(self):
+        return str(self.category)
+
+    
+    
+class Product(models.Model):
+    id_product       = models.AutoField(db_column='idProduct', primary_key=True)
+    products         = models.CharField(max_length=20, blank=True, null=True) 
+    id_category      = models.ForeignKey('Category',on_delete=models.CASCADE, db_column='idCategory')
+    image            = models.ImageField(upload_to="productos", null=True)
+    description      = models.CharField(max_length=150)  
+    activo           = models.BooleanField(default=True)
+
+    
+class Foundation(models.Model):
+    id_foundation    = models.AutoField(db_column='idFoundation', primary_key=True)
+    foundations      = models.CharField(max_length=20, blank=True, null=True) 
+    image            = models.ImageField(upload_to="fundaciones", null=True)
+    description      = models.CharField(max_length=450)
+    link             = models.CharField(max_length=50)  
+    activo           = models.BooleanField(default=True)
+
